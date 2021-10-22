8ec1a18232d867f8ef8e2eb41150dd4e3666597b
Marcos Rocha <marcosrocha.br@hotmail.com>
2021-10-22 09:52:25 -0300

    inicio

---
+++ iniciar-dia.c
@@ -0,0 +1,15 @@
+#include <windows.h>
+
+int main(){
+
+    //Digital Innovation One
+    system("start firefox https://digitalinnovation.one/");
+
+    //Web aula da Estácio
+    system("start firefox https://estacio.webaula.com.br/");
+
+    //Engenharia de software Univesp
+    system("start firefox https://youtu.be/kO1PSkzTsYc");
+
+    return 0;
+}
