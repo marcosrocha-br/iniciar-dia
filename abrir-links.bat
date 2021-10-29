0a84ca25aaea727b822e65c8b0547eeb76db72bd
Marcos Rocha <marcosrocha.br@hotmail.com>
2021-10-28 22:48:57 -0300

    add programas

--- abrir-links.bat
+++ abrir-links.bat
@@ -1,10 +1,25 @@
+		:: SITES
+
 :: DIGITAL INNOVATION ONE
 set dio=https://digitalinnovation.one/sign-in
 
 :: ESTÁCIO
 set estacio=https://estacio.webaula.com.br/
 
-::YOUTUBE
+:: YOUTUBE
 set youtube=https://youtube.com
 
-start chrome %dio% %estacio% %youtube%
+:: Abrir sites
+	start chrome %dio% %estacio% %youtube%
+
+
+
+		:: PROGRAMAS
+
+:: NETBEANS
+start netbeans.exe --console suppress
+
+:: FREEPLANE
+start freeplane.exe
+
+
