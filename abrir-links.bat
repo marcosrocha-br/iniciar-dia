bd3c70052ba23636f5799c4deab85c45dc898ae7
Marcos Rocha <marcosrocha.br@hotmail.com>
2021-10-28 22:34:12 -0300

    arquivo.bat

---
+++ abrir-links.bat
@@ -0,0 +1,10 @@
+:: DIGITAL INNOVATION ONE
+set dio=https://digitalinnovation.one/sign-in
+
+:: ESTÁCIO
+set estacio=https://estacio.webaula.com.br/
+
+::YOUTUBE
+set youtube=https://youtube.com
+
+start chrome %dio% %estacio% %youtube%
